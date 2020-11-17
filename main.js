6861172d394d610be48550abedb55c7639385730
Maher Masoud <muhran777@gmail.com>
2020-11-17 23:41:54 +0400

    markdown text area

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("HELLO")
+const textEditor = document.querySelector(".text-editor")
+const preview = document.querySelector(".preview")
+var showdown = require('showdown')
+
+const converter = new showdown.Converter()
+
+textEditor.addEventListener("keyup", event => {
+    const { value } = event.target;
+
+    const html = converter.makeHtml(value);
+
+    preview.innerHTML = html;
+    
+})
