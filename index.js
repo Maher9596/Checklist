a10acf4b8dcf41878bb2f1a0cd6e321a2be9b175
Ma7eer <maher.alkendi2@gmail.com>
2020-12-04 21:32:01 +0400

    Add eslint to the project and enforced airbnb rules + ran eslint on index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const PORT = 1234
+const express = require('express');
+
+const app = express();
+const PORT = 1234;
 
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    try {
-        res.redirect('/login')
-    } catch (error) {
-        console.log(error)
-    }
-})
+  try {
+    res.redirect('/login');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.get('/login', async (req, res) => {
-    try {
-        res.render('pages/login.ejs')
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    res.render('pages/login.ejs');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-})
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`))
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
